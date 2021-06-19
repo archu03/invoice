08c1503dc8ec76cb7d23e26239900f89c06bf5e1
archana35 <archanaraajendran@gmail.com>
2021-06-19 22:04:42 +0530

    final commit

---
+++ src/components/Invoice/Invoice.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react'
+import styles from './Invoice.module.scss'
+
+import LineItems from './LineItems'
+
+import uuidv4 from 'uuid/v4'
+
+class Invoice extends Component {
+
+  locale = 'en-US'
+  currency = 'USD'
+
+  state = {
+    taxRate: 0.00,
+    lineItems: [
+      {
+        id: 'initial',      // react-beautiful-dnd unique key
+        name: '',
+        description: '',
+        quantity: 0,
+        price: 0.00,
+      },
+    ]
+
+  }
+
+  handleInvoiceChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleLineItemChange = (elementIndex) => (event) => {
+    let lineItems = this.state.lineItems.map((item, i) => {
+      if (elementIndex !== i) return item
+      return {...item, [event.target.name]: event.target.value}
+    })
+    this.setState({lineItems})
+  }
+
+  handleAddLineItem = (event) => {
+    this.setState({
+      // use optimistic uuid for drag drop; in a production app this could be a database id
+      lineItems: this.state.lineItems.concat(
+        [{ id: uuidv4(), name: '', description: '', quantity: 0, price: 0.00 }]
+      )
+    })
+  }
+
+  handleRemoveLineItem = (elementIndex) => (event) => {
+    this.setState({
+      lineItems: this.state.lineItems.filter((item, i) => {
+        return elementIndex !== i
+      })
+    })
+  }
+
+  handleReorderLineItems = (newLineItems) => {
+    this.setState({
+      lineItems: newLineItems,
+    })
+  }
+
+  handleFocusSelect = (event) => {
+    event.target.select()
+  }
+
+  handlePayButtonClick = () => {
+    alert('Not implemented')
+  }
+
+  formatCurrency = (amount) => {
+    return (new Intl.NumberFormat(this.locale, {
+      style: 'currency',
+      currency: this.currency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).format(amount))
+  }
+
+  calcTaxAmount = (c) => {
+    return c * (this.state.taxRate / 100)
+  }
+
+  calcLineItemsTotal = () => {
+    return this.state.lineItems.reduce((prev, cur) => (prev + (cur.quantity * cur.price)), 0)
+  }
+
+  calcTaxTotal = () => {
+    return this.calcLineItemsTotal() * (this.state.taxRate / 100)
+  }
+
+  calcGrandTotal = () => {
+    return this.calcLineItemsTotal() + this.calcTaxTotal()
+  }
+
+  render = () => {
+    return (
+
+      <div className={styles.invoice}>
+        <div className={styles.brand}>
+	  <h1 className={styles.logo} >INVOICE</h1>
+        </div>
+        <div className={styles.addresses}>
+          <div className={styles.from}>
+            <strong>Amazing Company</strong><br />
+              123 abc street<br />
+              Canada <br />
+              416-555-3216
+          </div>
+          <div>
+            <div className={`${styles.valueTable} ${styles.to}`}>
+              <div className={styles.row}>
+                <div className={styles.label}>Customer #</div>
+                <div className={styles.value}>123456</div>
+              </div>
+              <div className={styles.row}>
+                <div className={styles.label}>Invoice #</div>
+                <div className={styles.value}>123456</div>
+              </div>
+              <div className={styles.row}>
+                <div className={styles.label}>Date</div>
+                <div className={`${styles.value} ${styles.date}`}>19-06-2021</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h2>Invoice</h2>
+
+          <LineItems
+            items={this.state.lineItems}
+            currencyFormatter={this.formatCurrency}
+            addHandler={this.handleAddLineItem}
+            changeHandler={this.handleLineItemChange}
+            focusHandler={this.handleFocusSelect}
+            deleteHandler={this.handleRemoveLineItem}
+            reorderHandler={this.handleReorderLineItems}
+          />
+
+        <div className={styles.totalContainer}>
+          <form>
+            <div className={styles.valueTable}>
+              <div className={styles.row}>
+                <div className={styles.label}>Tax Rate (%)</div>
+                <div className={styles.value}><input name="taxRate" type="number" step="0.01" value={this.state.taxRate} onChange={this.handleInvoiceChange} onFocus={this.handleFocusSelect} /></div>
+              </div>
+            </div>
+          </form>
+          <form>
+            <div className={styles.valueTable}>
+              <div className={styles.row}>
+                <div className={styles.label}>Subtotal</div>
+                <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcLineItemsTotal())}</div>
+              </div>
+              <div className={styles.row}>
+                <div className={styles.label}>Tax ({this.state.taxRate}%)</div>
+                <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcTaxTotal())}</div>
+              </div>
+              <div className={styles.row}>
+                <div className={styles.label}>Total Due</div>
+                <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcGrandTotal())}</div>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <br></br>
+	<br></br>
+
+        <div className={styles.footer}>
+          <div className={styles.comments}>
+            <h4>Notes</h4>
+            <p>Review Invoices when you receive them: Check the invoice due date , Review the goods or services provided , Check the total cost.</p>
+            <p>Choose the right payment method.</p>
+            <p>Organize invoices according to a payment schedule.</p>
+	    <p>Set Remainders.</p>
+	    
+          </div>
+         
+        </div>
+	<div className={styles.pay}>
+          <button className={styles.payNow} onClick={this.handlePayButtonClick}>Send Email</button>
+        </div>
+
+      </div>
+
+    )
+  }
+
+}
+
+export default Invoice
